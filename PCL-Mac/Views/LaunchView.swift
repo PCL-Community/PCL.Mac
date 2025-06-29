c059be24b785cb46745908e2a40eecc30ee18ff6
YiZhiMCQiu <yizhimcqiu@icloud.com>
2025-06-30 07:47:01 +0800

    feat(ui): 添加头像显示

--- PCL-Mac/Views/LaunchView.swift
+++ PCL-Mac/Views/LaunchView.swift
@@ -16,9 +16,14 @@ fileprivate struct LeftTab: View {
     var body: some View {
         VStack {
             Spacer()
-            Text(accountManager.getAccount()?.name ?? "无账号")
-                .font(.custom("PCL English", size: 16))
-                .foregroundStyle(Color("TextColor"))
+            if let account = accountManager.getAccount() {
+                MinecraftAvatarComponent(type: .username, src: account.name)
+                Text(account.name)
+                    .font(.custom("PCL English", size: 16))
+                    .foregroundStyle(Color("TextColor"))
+            } else {
+                Text("无账号")
+            }
             Spacer()
             if let instance = self.instance {
                 MyButtonComponent(text: "启动游戏", descriptionText: instance.config.name, foregroundStyle: AppSettings.shared.theme.getTextStyle()) {
